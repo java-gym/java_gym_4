3b7317e956da9b2a209da1a98115f259bb02def1
Suttorp <jeroen.suttorp@gmail.com>
2019-11-29 22:57:38 +0100

    eerste oplossing langs de wand

--- src/main/java/javagym/Solution.java
+++ src/main/java/javagym/Solution.java
@@ -8,9 +8,9 @@ import noedit.Maze;
 import noedit.Path;
 import noedit.PathBuilder;
 import noedit.Position;
+import org.w3c.dom.css.Counter;
 
-import static noedit.Cell.Exit;
-import static noedit.Cell.Wall;
+import static noedit.Cell.*;
 
 public class Solution {
 
@@ -22,18 +22,187 @@ public class Solution {
 
         System.out.println(maze.asStringAll());
         PathBuilder path = new PathBuilder(initialPosition);
+        Previous prev = Previous.right;
+
         while (true) {
+
+            //Alle posities bekijken die mogelijk zijn.
+            Position rightPos = path.latest().right();
+            Cell rightCell = maze.getOrElse(rightPos, Wall);
+            Position downPos = path.latest().down();
+            Cell downCell = maze.getOrElse(downPos, Wall);
+            Position upPos = path.latest().up();
+            Cell upCell = maze.getOrElse(upPos, Wall);
             Position leftPos = path.latest().left();
             Cell leftCell = maze.getOrElse(leftPos, Wall);
-            if (leftCell == Wall) {
-                // Let's just give up here.
+            //--TO DO:  Als er geen x in de maze zit dan moet er een break uitgevoerd worden.
+
+            if (downCell == Wall && leftCell == Wall && rightCell == Wall && upCell == Wall) {
                 break;
             }
-            path.left();
+            if (downCell == Exit) {
+                path.down();
+                break;
+            }
+            if (upCell == Exit) {
+                path.up();
+                break;
+            }
             if (leftCell == Exit) {
+                path.left();
                 break;
             }
+            if (rightCell == Exit) {
+                path.right();
+                break;
+            }
+
+            switch (prev) {
+
+                case right: {
+                    if (downCell == Open) {
+                        path.down();
+                        prev = Previous.down;
+                        continue;
+                    }
+                    if (rightCell == Wall) {
+                        if (upCell == Wall) {
+                            if (leftCell == Wall) {
+                                if (downCell == Wall) {
+                                    System.out.println("WE WILL NOT COME OUT THIS MAZE!!");
+                                    break;
+                                } else {
+                                    path.down();
+                                    prev = Previous.down;
+                                    continue;
+                                }
+                            } else {
+                                path.left();
+                                prev = Previous.left;
+                                continue;
+                            }
+                        } else {
+                            path.up();
+                            prev = Previous.up;
+                            continue;
+                        }
+                    } else {
+                        path.right();
+                        prev = Previous.right;
+                        continue;
+                    }
+                }
+                case up: {
+                    if (rightCell == Open) {
+                        path.right();
+                        prev = Previous.right;
+                        continue;
+                    }
+                    if (upCell == Wall) {
+                        if (leftCell == Wall) {
+                            if (downCell == Wall) {
+                                if (rightCell == Wall) {
+                                    System.out.println("WE WILL NOT COME OUT THIS MAZE!!");
+                                    break;
+                                } else {
+                                    path.right();
+                                    prev = Previous.right;
+                                    continue;
+                                }
+                            } else {
+                                path.down();
+                                prev = Previous.down;
+                                continue;
+                            }
+                        } else {
+                            path.left();
+                            prev = Previous.left;
+                            continue;
+                        }
+                    } else {
+                        path.up();
+                        prev = Previous.up;
+                        continue;
+                    }
+                }
+                case down: {
+                    if (leftCell == Open) {
+                        path.left();
+                        prev = Previous.left;
+                        continue;
+                    }
+                    if (downCell == Wall) {
+                        if (rightCell == Wall) {
+                            if (upCell == Wall) {
+                                if (leftCell == Wall) {
+                                    System.out.println("WE WILL NOT COME OUT THIS MAZE!!");
+                                    break;
+                                } else {
+                                    path.left();
+                                    prev = Previous.left;
+                                    continue;
+                                }
+                            } else {
+                                path.up();
+                                prev = Previous.up;
+                                continue;
+                            }
+                        } else {
+                            path.right();
+                            prev = Previous.right;
+                            continue;
+                        }
+                    } else {
+                        path.down();
+                        prev = Previous.down;
+                        continue;
+                    }
+
+                }
+                case left: {
+                    if (upCell == Open) {
+                        path.up();
+                        prev = Previous.up;
+                        continue;
+                    }
+                    if (leftCell == Wall) {
+                        if (downCell == Wall) {
+                            if (rightCell == Wall) {
+                                if (upCell == Wall) {
+                                    System.out.println("WE WILL NOT COME OUT THIS MAZE!!");
+                                    break;
+                                } else {
+                                    path.up();
+                                    prev = Previous.up;
+                                    continue;
+                                }
+                            } else {
+                                path.right();
+                                prev = Previous.right;
+                                continue;
+                            }
+                        } else {
+                            path.down();
+                            prev = Previous.down;
+                            continue;
+                        }
+                    } else {
+                        path.left();
+                        prev = Previous.left;
+                        continue;
+                    }
+
+
+                }
+            }
+
+
+
         }
         return path.build();
     }
+}
+
+enum Previous {
+    left, right, up, down;
 }
